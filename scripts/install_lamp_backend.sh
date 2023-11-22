6d7a7697d5323891fe5fde7829735a41e0362f02
Ubuntu <ubuntu@ip-172-31-91-133.ec2.internal>
2023-11-22 13:15:45 +0000

    ll

--- scripts/install_lamp_backend.sh
+++ scripts/install_lamp_backend.sh
@@ -7,6 +7,6 @@ apt-get upgrade -y
 
 apt-get install mysql-server -y
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/127.0.0.1/$WORDPRESS_DB_HOST/" /etc/mysql/mysql.conf.d/mysqld.cnf
 
 systemctl restart mysql
